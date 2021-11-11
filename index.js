e6a75fe75bbfdd5293ef58c7cffe8ed1a7e73fec
RandallHammond23 <hrandall31@gmail.com>
2021-11-10 19:01:43 -0600

    add get request for a single song.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const repoContext = require('./repository/repository-wrapper');
 
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.listen(3000, function (){
@@ -11,8 +13,15 @@ app.listen(3000, function (){
 
 
 app.get('/api/songs', (req, res) => {
- const songs = repoContext.products.findAllSongs();
+ const songs = repoContext.songs.findAllSongs();
  return res.send(songs);
 });
 
+app.get('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const songs = repoContext.songs.findSongById(id);
+    return res.send(songs);
+   });
 
+   
+   
