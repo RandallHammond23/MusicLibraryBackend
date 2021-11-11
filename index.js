84c5eb7c43bdb392c0a8e1fb2e5cc72e46c03db5
RandallHammond23 <hrandall31@gmail.com>
2021-11-10 19:49:59 -0600

    add Cross-Origin Resource Sharing to project.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const repoContext = require('./repository/repository-wrapper');
+const cors = require('cors');
 const { validateSong } = require('./middleware/songs-validation');
 
 
+app.use(cors());
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
