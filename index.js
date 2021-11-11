d7537c66dbb1592a0052011b60b14b01bf0963e0
RandallHammond23 <hrandall31@gmail.com>
2021-11-10 18:32:37 -0600

    add get all songs route.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const repoContext = require('./repository/repository-wrapper');
 
 
 const app = express();
@@ -6,5 +7,12 @@ const app = express();
 
 app.listen(3000, function (){
     console.log("Server started. Listening on port 3000.");
-    
-    });
+     });
+
+
+app.get('/api/songs', (req, res) => {
+ const songs = repoContext.products.findAllSongs();
+ return res.send(songs);
+});
+
+
